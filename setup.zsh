1f6f1dad62a775931c155cd3ebd05c190cc19978
Felix D <flx.descoteaux@gmail.com>
2014-07-11 17:25:31 -0400

    added setup script

--- setup.zsh
+++ setup.zsh
@@ -1,8 +1,9 @@
 #! /bin/zsh
 curr_path="$PWD"
-ln -s "${PWD}/prezto" "${HOME}/.zprezto"
-cd "${HOME}/.zprezto"
+ln -s "${PWD}"/.zprezto "${HOME}"/.zprezto
+cd "${HOME}"/.zprezto
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+echo "alias vi='mvim -v'\n" >> ${HOME}/.zshrc
