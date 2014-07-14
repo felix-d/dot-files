96afa17d106e3677bb6a2bc0a1026942669e23ee
Felix D <flx.descoteaux@gmail.com>
2014-07-14 09:05:38 -0400

    git cache cleared

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/Users/felix/dot-files/.vim
--- setup.zsh
+++ setup.zsh
@@ -1,9 +1,18 @@
 #! /bin/zsh
-curr_path="$PWD"
-ln -s "${PWD}"/.zprezto "${HOME}"/.zprezto
+[ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
+
+git_branch='3.0'
+files=(".vim" ".vimrc" ".tmux.conf" ".zprezto")
+for file in "${files[@]}"; do
+  ln -Fs "${PWD}/${file}" "${HOME}/${file}"
+done
 cd "${HOME}"/.zprezto
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    ln -Fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-echo "alias vi='mvim -v'\n" >> ${HOME}/.zshrc
+echo "alias vim='mvim -v'\n" >> ${HOME}/.zshrc
+
+mkdir -p "{$HOME}/.vim/bundle/"
+git clone $VUNDLE_URI "$HOME/.vim/bundle/Vundle.git"
+vim +PluginInstall +qall
